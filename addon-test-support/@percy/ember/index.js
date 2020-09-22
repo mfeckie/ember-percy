2b866d373097c660defecfc9c97008864486c62d
Wil Wilsman <wil@wilwilsman.com>
2020-09-22 18:02:20 -0500

    ✨ Use @percy/cli

--- addon-test-support/@percy/ember/index.js
+++ addon-test-support/@percy/ember/index.js
@@ -1,141 +1,105 @@
-let isPercyRunning = true;
-let agentJS;
+import serializeDOM from '@percy/dom'
+
+// Collect client and environment information
+import { VERSION as sdkVersion } from '@percy/ember/version';
+import { VERSION as emberVersion } from '@ember/version';
+
+const CLIENT_INFO = `@percy/ember/${sdkVersion}`;
+const ENV_INFO = [`ember/${emberVersion}`];
+
+if (window.QUnit) ENV_INFO.push(`qunit/${window.QUnit.version}`);
+if (window.mocha) ENV_INFO.push(`mocha/${window.mocha.version}`);
+
+// Maybe get the CLI API address from the environment
+import { PERCY_CLI_API } from '@percy/ember/cli-env';
+
 // Capture fetch before it's mutated by Pretender
-let PercyFetch = window.fetch;
+const { fetch } = window;
 
-async function fetchDOMLib() {
-  if (agentJS) return agentJS;
-
-  try {
-    return await PercyFetch('http://localhost:5338/percy-agent.js').then(res => res.text());
-  } catch (err) {
-    console.log(`[percy] Error fetching DOM library, disabling: ${err}`);
-    isPercyRunning = false;
-  }
-}
-
-function envInfo() {
-  function frameworkVersion() {
-    if (window.QUnit) {
-      return `qunit/${window.QUnit.version}`;
-    } else if (window.Mocha) {
-      // Doesn't look easy to grab the version while in the browser
-      return `mocha/unknown`;
+// Check if Percy is enabled using the healthcheck endpoint
+export async function isPercyEnabled() {
+  if (isPercyEnabled.result == null) {
+    try {
+      let response = await fetch(`${PERCY_CLI_API}/healthcheck`);
+      isPercyEnabled.result = response.ok;
+    } catch (err) {
+      isPercyEnabled.result = false;
     }
 
-    return 'unknown';
+    if (isPercyEnabled.result === false) {
+      console.log('[percy] Percy is not running, disabling snapshots');
+    }
   }
 
-  return `ember/${window.Ember.VERSION}; ${frameworkVersion()};`;
+  return isPercyEnabled.result;
 }
 
-function clientInfo() {
-  return `@percy/ember@v2.1.4`;
+// Helper to generate a snapshot name from the test suite
+function generateName(assertOrTestOrName) {
+  if (assertOrTestOrName.test?.module?.name && assertOrTestOrName.test?.testName) {
+    // generate name from qunit assert object
+    return `${assertOrTestOrName.test.module.name} | ${assertOrTestOrName.test.testName}`;
+  } else if (assertOrTestOrName.fullTitle) {
+    // generate name from mocha test object
+    return assertOrTestOrName.fullTitle();
+  } else {
+    // fallback to string
+    return assertOrTestOrName.toString();
+  }
 }
 
-// This will only remove the transform applied by Ember's defaults
-// If there are custom styles applied, use Percy CSS to overwrite
-function removeEmberTestStyles(dom) {
+// Helper to scope a DOM snapshot to the ember-testing container
+function scopeDOM(dom, { scope, domTransformation }) {
+  if (domTransformation) domTransformation(dom);
+  // we only want to capture the ember application, not the testing UI
+  let $scoped = dom.querySelector(scope || '#ember-testing');
+  let $body = dom.querySelector('body');
+  if (!$scoped) return;
+
+  // replace body content with scoped content
+  $body.innerHTML = $scoped.innerHTML;
+
+  // copy scoped attributes to the body element
+  for (let i = 0; i < $scoped.attributes.length; i++) {
+    let { name, value } = $scoped.attributes.item(i);
+    // keep any existing body class
+    if (name === 'class') value = `${$body.className} ${value}`.trim();
+    $body.setAttribute(name, value);
+  }
+
+  // remove ember-testing styles by removing the id
   dom.querySelector('#ember-testing').removeAttribute('id');
 }
 
-function autoGenerateName(name) {
-  // Automatic name generation for QUnit tests by passing in the `assert` object.
-  if (name.test && name.test.module && name.test.module.name && name.test.testName) {
-    return `${name.test.module.name} | ${name.test.testName}`;
-  } else if (name.fullTitle) {
-    // Automatic name generation for Mocha tests by passing in the `this.test` object.
-    return name.fullTitle();
-  } else {
-    return name;
-  }
-}
-
 export default async function percySnapshot(name, options = {}) {
-  // Skip if Testem is not available (we're probably running from `ember server` and Percy is not
-  // enabled anyway).
-  if (!window.Testem) {
-    return;
+  if (!(await isPercyEnabled())) return;
+  name = generateName(name);
+
+  try {
+    // Serialize and capture the DOM
+    let domSnapshot = serializeDOM({
+      enableJavaScript: options.enableJavaScript,
+      domTransformation: dom => scopeDOM(dom, options)
+    });
+
+    // Post the DOM to the snapshot endpoint with snapshot options and other info
+    let response = await fetch(`${PERCY_CLI_API}/snapshot`, {
+      method: 'POST',
+      body: JSON.stringify({
+        ...options,
+        environmentInfo: ENV_INFO,
+        clientInfo: CLIENT_INFO,
+        url: document.URL,
+        domSnapshot,
+        name
+      })
+    });
+
+    // Handle errors
+    let { success, error } = await response.json();
+    if (!success) throw new Error(error);
+  } catch (err) {
+    console.error(`[percy] Could not take DOM snapshot "${name}"`);
+    console.error(`[percy] ${err.stack}`);
   }
-
-  if (!isPercyRunning) {
-    return false;
-  }
-
-  // cache the JS lib
-  agentJS = await fetchDOMLib();
-  if (!agentJS) return;
-
-  let scopedSelector = options.scope || '#ember-testing';
-  let $script = document.querySelector('.percy-agent-js');
-
-  if (!$script) {
-    $script = document.createElement('script');
-    $script.classList.add('percy-agent-js');
-    $script.innerText = agentJS;
-    document.body.appendChild($script);
-  }
-
-  // This takes the embeded Ember apps DOM and hoists it
-  // up and out of the test output UI. Without this Percy
-  // would capture the Ember test output too
-  function hoistAppDom(dom) {
-    let $scopedRoot = dom.querySelector(scopedSelector);
-    let $body = dom.querySelector('body');
-    let bodyClass = $body.getAttribute('class') || '';
-
-    $body.innerHTML = $scopedRoot.innerHTML;
-
-    // Copy over the attributes from the ember applications root node
-    for (let i = 0; i < $scopedRoot.attributes.length; i++) {
-      let attr = $scopedRoot.attributes.item(i);
-      // Merge the two class lists
-      if (attr.nodeName === 'class') {
-        $body.setAttribute('class', `${bodyClass} ${attr.nodeValue}`);
-      } else {
-        $body.setAttribute(attr.nodeName, attr.nodeValue);
-      }
-    }
-
-    removeEmberTestStyles(dom);
-    return dom;
-  }
-
-  let domSnapshot = new window.PercyAgent({
-    handleAgentCommunication: false,
-    // We only want to capture the ember application, not the testing UI
-    domTransformation: function(clonedDom) {
-      if (!clonedDom.querySelector(scopedSelector)) {
-        return clonedDom;
-      }
-
-      if (options.domTransformation) {
-        options.domTransformation(clonedDom);
-      }
-
-      return hoistAppDom(clonedDom);
-    }
-  }).domSnapshot(document, options);
-
-  // Must be awaited on or you run the risk of doing asset discovery
-  // when the ember server has already shut down
-  await PercyFetch('http://localhost:5338/percy/snapshot', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      clientInfo: clientInfo(),
-      environmentInfo: envInfo(),
-      url: document.URL,
-      domSnapshot,
-      name: autoGenerateName(name),
-      ...options
-    })
-  }).catch(err => {
-    if (isPercyRunning) {
-      console.log(`[percy] Error POSTing DOM, disabling: ${err}`);
-      isPercyRunning = false;
-    }
-  });
 }
--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 'use strict';
 
+const WriteFile = require('broccoli-file-creator');
+const MergeTrees = require('broccoli-merge-trees');
 const BroccoliDebug = require('broccoli-debug');
 const debugTree = BroccoliDebug.buildDebugCallback('@percy/ember');
+const pkg = require('./package');
+
+// Maybe get the CLI API address from the environment
+const { PERCY_CLI_API = 'http://localhost:5338/percy' } = process.env;
 
 module.exports = {
-  name: require('./package').name,
-  // Inspired by `@ember/test-helpers`:
-  // https://github.com/emberjs/ember-test-helpers/blob/master/index.js#L28-L57
-  treeForAddonTestSupport(tree) {
-    // intentionally not calling _super here
-    // _super would return same tree but with namespace including the sub folder
-    let input = debugTree(tree, 'addon-test-support:input');
-    let output = this.preprocessJs(input, '/', this.name, {
-      registry: this.registry
-    });
+  name: pkg.name,
 
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('node_modules/@percy/dom/dist/index.js', {
+      using: [{ transformation: 'amd', as: '@percy/dom' }],
+      type: 'test'
+    });
+  },
+
+  treeForAddonTestSupport(tree) {
+    let version = new WriteFile('@percy/ember/version.js', (
+      `export const VERSION = "${pkg.version}";\n`
+    ));
+
+    let env = new WriteFile('@percy/ember/cli-env.js', (
+      `export const PERCY_CLI_API = "${PERCY_CLI_API}";\n`
+    ));
+
+    let input = debugTree(new MergeTrees([tree, version, env]), 'addon-test-support:input');
+    let output = this.preprocessJs(input, '/', this.name, { registry: this.registry });
     return debugTree(output, 'addon-test-support:output');
   }
 };
